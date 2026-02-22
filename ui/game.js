be71f149643464d1548bde80c6cc914f9243f8c1
james-e-crandall <james.e.crandall@gmail.com>
2026-02-21 21:51:42 -0600

    name updates

---
+++ ui/game.js
@@ -0,0 +1,17 @@
+window.UnitPortraitVisualRemaps = {
+    ...(window.UnitPortraitVisualRemaps || {}),
+    'UNIT_TRANS_SAHARAN_TRADER_JEC': { //your unit type
+        unitType: 'UNIT_MERCHANT',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
+    'UNIT_TRANS_SAHARAN_TRADER_JEC_2': { //your unit type
+        unitType: 'UNIT_MERCHANT',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
+}
+
+
+
+
+
+
